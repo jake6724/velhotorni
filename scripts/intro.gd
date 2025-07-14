a3719f0388186368f88025986d17b03c5519e4b9
jake6724 <jakealdridge6724@gmail.com>
2025-07-14 13:33:24 -0400

    Add intro. Export HTLM build

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,6 +1,9 @@
 extends Node2D
 var main_menu: PackedScene = preload("res://scenes/MainMenu.tscn")
+
 func _ready() -> void:
+
 	$VideoStreamPlayer.finished.connect(on_intro_finished)
+
 func on_intro_finished() -> void:
-	get_tree().changed_scene_to_packed(main_menu)
+	get_tree().change_scene_to_packed(main_menu)
