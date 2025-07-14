2eda566c5088b426412847c6cbd7a8b818739565
jake6724 <jakealdridge6724@gmail.com>
2025-07-14 16:29:04 -0400

    Fix music player

--- scripts/music_player.gd
+++ scripts/music_player.gd
@@ -2,20 +2,23 @@
 extends Node
 
 var music_player: AudioStreamPlayer = AudioStreamPlayer.new()
-var track_1: AudioStreamOggVorbis = preload("res://audio/music/Theme-1_Auto.ogg")
-var track_2: AudioStreamOggVorbis = preload("res://audio/music/Theme_2_8bit.ogg")
-var active_track = track_1
 
 var bus_volume_linear: float = .4
 var bus_index: int
-
 var prev_player_volume_linear = .4
+
+var track_1: AudioStreamOggVorbis = preload("res://audio/music/Theme-1_Auto.ogg")
+var track_2: AudioStreamOggVorbis = preload("res://audio/music/Theme_2_8bit.ogg")
+var tracks: Array[AudioStreamOggVorbis] = [track_1, track_1, track_2]
+var active_track: AudioStreamOggVorbis
 
 signal fade_out_complete
 signal fade_in_complete
 
-
 func _ready():
+	# Keep music running MusicPlayer on pause
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 	# Configure music audio bus
 	bus_index = AudioServer.get_bus_index("music")
 	AudioServer.set_bus_volume_linear(bus_index, bus_volume_linear)
@@ -23,26 +26,16 @@ func _ready():
 	# Configure AudioStreamPlayer
 	music_player.bus = "music"
 	add_child(music_player)
+	update_active_track()
+
+func update_active_track():
+	fade_out()
+	await fade_out_complete
+	active_track = tracks[GameManager.level_index]
 	music_player.stream = active_track
 	music_player.play()
-
-	music_player.finished.connect(on_track_finished)
-
-	# Keep music running on pause
-	process_mode = Node.PROCESS_MODE_ALWAYS
-
-func on_track_finished():
-	await get_tree().create_timer(3).timeout
-	# Needs to be extended to work with more tracks
-	if active_track == track_1:
-		active_track = track_2
-		music_player.stream = active_track
-		music_player.play()
-
-	else:
-		active_track = track_1
-		music_player.stream = active_track
-		music_player.play()
+	fade_in()
+	await fade_in_complete
 
 func update_bus_volume(new_bus_volume_linear: float):
 	AudioServer.set_bus_volume_linear(bus_index, new_bus_volume_linear)
