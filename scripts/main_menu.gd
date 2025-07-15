4cebd369fc5028a051fbcd33f6a13e7d35f18f91
jake6724 <jakealdridge6724@gmail.com>
2025-07-14 23:02:30 -0400

    Add click sfx to play button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,11 +9,7 @@ func _ready() -> void:
 	start_button.pressed.connect(_on_play_button_pressed)
 
 func _on_play_button_pressed() -> void:
+	SFXPlayer.play_sfx("click_1")
 	GameManager.level_index = 0
 	GameManager.configure_active_level()
 	SceneTransition.change_scene(main_scene)
-	
-# func _on_tutorial_button_pressed() ->void :
-# 	GameManager.level_index = 0
-# 	GameManager.configure_active_level()
-# 	SceneTransition.change_scene(main_scene)
