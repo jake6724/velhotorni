0cd1aa9e0d82b68843253ac2bd1c9560d3d6db79
jake6724 <jakealdridge6724@gmail.com>
2025-07-22 10:28:43 -0400

    Fix base bugs

--- scripts/singletons/wave_manager.gd
+++ scripts/singletons/wave_manager.gd
@@ -41,6 +41,7 @@ func check_wave_complete() -> void:
 				on_wave_complete()
 
 func on_wave_complete() -> void:
+	print("WaveManager: ", wave_completed)
 	wave_index += 1
 	if wave_index < level_waves.size():
 		active_wave = level_waves[wave_index]
