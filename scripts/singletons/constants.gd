30cc9761e9f4e4bc6615180b0d0ac214462a605a
jake6724 <jakealdridge6724@gmail.com>
2025-07-23 10:40:54 -0400

    Bug fixes to enemydata

--- scripts/singletons/constants.gd
+++ scripts/singletons/constants.gd
@@ -2,7 +2,7 @@ extends Node
 
 enum Element {FIRE, NATURE, WATER, DARK, LIGHT, STORM, NONE}
 
-const FAST_FORWARD_SPEED: float = 20.0
+const FAST_FORWARD_SPEED: float = 2.0
 
 const CELL_SIZE: int = 16
 
