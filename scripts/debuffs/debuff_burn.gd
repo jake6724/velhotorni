768d8adf1b19446d4b1de623ec85c1bf82f541e1
jake6724 <jakealdridge6724@gmail.com>
2025-07-28 17:34:41 -0400

    Rework burn

--- scripts/debuffs/debuff_burn.gd
+++ scripts/debuffs/debuff_burn.gd
@@ -7,15 +7,19 @@ signal debuff_apply_burn
 ## Emitted with a value to increase `speed` by
 signal debuff_remove_burn
 
+var can_burn: bool = true
+
 func start_debuff() -> void:
 	debuff_apply_burn.emit(data.value, data.element)
 	total_timer.start(data.total_duration)
 	repeat_timer.start(data.repeat_duration)
 
 func on_repeat_timer_timeout() -> void:
-	debuff_apply_burn.emit(data.value, data.element)
-	repeat_timer.start(data.repeat_duration)
+	if can_burn:
+		debuff_apply_burn.emit(data.value, data.element)
+		repeat_timer.start(data.repeat_duration)
 
 func on_total_timer_timeout() -> void:
+	can_burn = false
 	debuff_remove_burn.emit(self)
 	queue_free()
