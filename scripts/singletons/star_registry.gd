4964ac1091e69598c1f26d120b6753f4ac47d695
jake6724 <jakealdridge6724@gmail.com>
2025-08-31 15:05:00 -0400

    Add global

--- scripts/singletons/star_registry.gd
+++ scripts/singletons/star_registry.gd
@@ -6,10 +6,10 @@ extends Node
 ## 3: 2 star
 ## 4: 3 star
 var stars: Dictionary[PackedScene, int] = {
-	LevelManager.level_0: 1, 
-	LevelManager.level_1: 0, 
-	LevelManager.level_2: 0, 
-	LevelManager.level_3a: 0, 
+	LevelManager.level_0: 2, 
+	LevelManager.level_1: 3, 
+	LevelManager.level_2: 4, 
+	LevelManager.level_3a: 2, 
 	LevelManager.level_3b: 0, 
 	LevelManager.level_4: 0, 
 	LevelManager.level_5: 0, 
@@ -36,3 +36,52 @@ var stars: Dictionary[PackedScene, int] = {
 	LevelManager.level_21: 0,
 	LevelManager.level_22: 0,
 }
+
+var region_stars: Array[int] = [0, 0, 0 ,0 ,0 ,0, 0, 0]
+
+func _ready():
+	update_star_progress()
+	print("Region stars: ", region_stars)
+
+func update_star_progress() -> void:
+	var i: int = 0
+	while i < (LevelManager.levels.size() - 1):
+
+		if i > 0 and i % 3 == 0 and i <= 18:
+			if stars[LevelManager.levels[i]] > 1 and stars[LevelManager.levels[i+2]] == 0:
+				stars[LevelManager.levels[i+2]] = 1
+
+		if stars[LevelManager.levels[i]] > 1 and stars[LevelManager.levels[i+1]] == 0:
+			stars[LevelManager.levels[i+1]] = 1
+
+		var star_count: int = stars[LevelManager.levels[i]]
+		update_region_stars(i, star_count)
+		i += 1
+
+func update_region_stars(index: int, star_count: int) -> void:
+
+	var region_index: int = get_region_index(index)
+	if region_index != -1:
+		region_stars[region_index] += star_count
+	else:
+		push_error("Region index invalid")
+
+func get_region_index(index: int) -> int:
+	if index == 0:
+		return 0
+	elif index <= 4:
+		return 1
+	elif index <= 8:
+		return 2
+	elif index <= 12:
+		return 3
+	elif index <= 16:
+		return 4
+	elif index <= 20:
+		return 5
+	elif index <= 24:
+		return 6
+	elif index <= 28:
+		return 7
+	else:
+		return -1
