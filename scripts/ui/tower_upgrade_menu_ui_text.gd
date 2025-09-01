9d9bfc78950055190554d06fbedd4dbf466594dd
Dreadway420 <spenceanator11@gmail.com>
2025-09-01 16:30:42 -0400

    Update ugrade menu text

--- scripts/ui/tower_upgrade_menu_ui_text.gd
+++ scripts/ui/tower_upgrade_menu_ui_text.gd
@@ -1,17 +1,17 @@
 class_name TowerUpgradeMenuUIText
 extends Object
 
-var damage_button_hovered: String = "Damage applied to an enemy on-hit"
-var speed_button_hovered: String = "Shots per second"
-var range_button_hovered: String = "The max range of this tower"
+var damage_button_hovered: String = "Damage applied to an enemy on-hit."
+var speed_button_hovered: String = "The number of shots fired each second."
+var range_button_hovered: String = "The radius of the tower's max range in pixels."
 
 var special_button_hovered: String = "Special placeholder text"
-var special_debuff_burn: String = "Upgrade burn damage \n\n Burn enemies every .5 seconds \n lasts for 6 seconds"
-var special_debuff_slow: String = "Upgrade slow duration \n\n "
-var special_debuff_freeze: String = "Upgrade freeze duration"
-var special_debuff_stun: String = "Upgrade stun duration"
-var special_debuff_knockback: String = "Upgrade knockback distance"
-var special_debuff_weaken: String = "Upgrade weaken duration"
+var special_debuff_burn: String = "Upgrades burn damage. \n Does not stack. \n Burning enemies take damage every .5 seconds. Lasts for 6 seconds."
+var special_debuff_slow: String = "Upgrades slow duration. \n Does not stack. \n stronger slows take priority over weaker ones."
+var special_debuff_freeze: String = "Upgrades freeze duration. \n Frozen enemies build resistance to freezes with each application."
+var special_debuff_stun: String = "Upgrades stun duration. \n Stunned enemies build resistance to stuns with each application."
+var special_debuff_knockback: String = "Upgrades knockback distance. \n Affected enemies build resistance to knockbacks with each application."
+var special_debuff_weaken: String = "Upgrades weaken duration. \n Does not stack. \n Weakened enemies take increased damage \n from all sources."
 var special_debuff_button_hovered_options: Dictionary[Debuff.Type, String] = {
 	Debuff.Type.BURN: special_debuff_burn,
 	Debuff.Type.SLOW: special_debuff_slow,
@@ -20,9 +20,9 @@ var special_debuff_button_hovered_options: Dictionary[Debuff.Type, String] = {
 	Debuff.Type.KNOCKBACK: special_debuff_knockback,
 	Debuff.Type.WEAKEN: special_debuff_weaken,}
 
-var special_buff_range: String = "Increases the range of allied towers"
-var special_buff_damage: String = "Increases the damage of allied towers"
-var special_buff_speed: String = "Increases the speed of allied towers"
+var special_buff_range: String = "Increases the range of allied towers. Does not affect itself nor other light towers."
+var special_buff_damage: String = "Increases the damage of allied towers. Does not affect itself nor other light towers."
+var special_buff_speed: String = "Increases the speed of allied towers. Does not affect itself nor other light towers."
 var special_buff_button_hovered_options: Dictionary[Buff.Type, String] = {
 	Buff.Type.DAMAGE: special_buff_damage,
 	Buff.Type.RANGE: special_buff_range,
@@ -34,10 +34,10 @@ var special_bullet_modifier_button_hovered_options: Dictionary[BulletModifierDat
 	BulletModifierData.Type.COIN: special_bullet_modifier_coin,
 }
 
-var targeting_priority_first: String = "Target the enemy farthest along on the path"
-var targeting_priority_last: String = "Target the enemy closest to the spawn point"
-var targeting_priority_highest: String = "Target the enemy with the highest current health"
-var targeting_priority_lowest: String = "Target the enemy with the lowest current health"
+var targeting_priority_first: String = "Target the enemy farthest along on the path."
+var targeting_priority_last: String = "Target the enemy closest to the start of the path."
+var targeting_priority_highest: String = "Target the enemy with the highest current health."
+var targeting_priority_lowest: String = "Target the enemy with the lowest current health."
 var targeting_hovered_options: Dictionary[Tower.TargetPriority, String] = {
 	Tower.TargetPriority.FIRST: targeting_priority_first,
 	Tower.TargetPriority.LAST: targeting_priority_last,
@@ -45,6 +45,6 @@ var targeting_hovered_options: Dictionary[Tower.TargetPriority, String] = {
 	Tower.TargetPriority.LOWEST: targeting_priority_lowest,
 }
 
-var requirements_hovered: String = "Cost to upgrade 1 stat for this tower"
-var level_hovered: String = "The current and next level of this tower"
+var requirements_hovered: String = "Cost to upgrade 1 stat for this tower."
+var level_hovered: String = "The current and next level of this tower."
 var level_hovered_maxed: String = "Max level reached!"
