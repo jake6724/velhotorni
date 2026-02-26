dd7f68ce04a506d039c9b23d397cc7f2fd771bb3
Dreadway420 <spenceanator11@gmail.com>
2026-02-26 12:35:10 -0500

    3b rebalance

--- scripts/singletons/player_loadout.gd
+++ scripts/singletons/player_loadout.gd
@@ -43,12 +43,12 @@ var spells: Dictionary[SpellData, bool] = {
 
 # TOWERS ###################################################################################################################
 # Preload started causing issues after I added the bullet_modifier stuff in TowerGlobal data (and other related scripts such as bullet) when working on perk_data_tower_bullet_modifier_coin. Likely caused a cyclical dependency
-var equipped_tower_1: TowerData = load("res://data/towers/tower_data_fire_plasma.tres")
+var equipped_tower_1: TowerData = load("res://data/towers/tower_data_water_ice.tres")
 var equipped_tower_2: TowerData = load("res://data/towers/tower_data_wind_lightning.tres")
 var equipped_tower_3: TowerData = load("res://data/towers/tower_data_arcane.tres")
-var equipped_tower_4: TowerData = null
-var equipped_tower_5: TowerData = null
-var equipped_tower_6: TowerData = null
+var equipped_tower_4: TowerData = load("res://data/towers/tower_data_fire_lava.tres")
+var equipped_tower_5: TowerData = load("res://data/towers/tower_data_light_spirit.tres")
+var equipped_tower_6: TowerData = load("res://data/towers/tower_data_dark.tres")
 var equipped_towers: Array[TowerData] = [equipped_tower_1, equipped_tower_2, equipped_tower_3, 
 equipped_tower_4, equipped_tower_5, equipped_tower_6]
 
